76f12e87a46ab11330a82e3259649e4c094b1bcf
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2025-08-16 22:48:38 +0000

    stage0, installing, prepocesing and so on...

---
+++ pet-planner-pwa/src/views/SplashScreen.jsx
@@ -0,0 +1,14 @@
+//pet-planner-pwa/src/views/SplashScreen.jsx
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <img src="/logo.png" alt="Logo Pet Planner" style={{ width: '120px' }} />
+      <h1>🐾 Pet Planner</h1>
+      <p>Organizá la salud de tus mascotas. Sin conexión. Sin complicaciones.</p>
+    </div>
+  );
+};
+
+export default SplashScreen;
